8222002b7b5bf084c5aeb034176e8a4c4e621885
Micah R Ledbetter <micah@micahrl.com>
2013-04-24 14:42:48 -0500

    solution for challenge 5, whee

--- c05_rkeyxor.py
+++ c05_rkeyxor.py
@@ -18,20 +18,23 @@ plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear 
 repkey = "ICE"
 solution = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
+# Note that I pad the plaintext with chr(0x00) at the end rather than cutting down the key to fit the plaintext
+# ... this is because if I do the latter, I end up with the same result except without "2f" at the end of
+# the solution. 
 def repxor(plaintext, key):
-    """This function does what I expect, except that the solution has "2f" at the end where mine has nothing.
-    Is this because I'm chopping off the key instead of padding the plaintext at the end?"""
-    if len(plaintext) < len(key):
-        fullkey = key[0:len(plaintext)]
-    elif len(key) < len(plaintext):
+    fullkey = key
+    if len(key) < len(plaintext):
         remainder = len(plaintext) - len(key)
         iterate = int(remainder/len(key))+1 # the +1 catches a remainder, if any; I cut it down later.
         fullkey=key
         for i in range(iterate):
             fullkey += key
-        fullkey = fullkey[0:len(plaintext)]
-    else:
-        fullkey = key
+
+    if len(plaintext) < len(fullkey):
+        remainder = len(fullkey) - len(plaintext)
+        for i in range(remainder):
+            plaintext += chr(0x00)
+
 
     hextxt = c01_hexb64.string_to_hex(plaintext)
     hexkey = c01_hexb64.string_to_hex(fullkey)
