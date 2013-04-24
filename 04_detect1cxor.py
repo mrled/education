00c7415db197e1238c588202e6c8aebbdf837611
Micah R Ledbetter <mrled@younix.us>
2013-04-23 22:26:48 -0500

    added initial files for challenge 4

---
+++ 04_detect1cxor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# One of the 60-character strings at:
+
+#   https://gist.github.com/3132713
+
+# has been encrypted by single-character XOR. Find it. (Your code from
+# #3 should help.)
+
+
