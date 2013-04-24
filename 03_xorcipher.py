fc9d9a44d2bf9e32479c93f2f207267f6045be29
Micah R Ledbetter <mrled@younix.us>
2013-04-23 22:24:34 -0500

    solved challenge 3 :)

--- 03_xorcipher.py
+++ 03_xorcipher.py
@@ -19,11 +19,13 @@ ciphertext = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b37
 def score_plaintext(text):
     vowels = 'aeiouAEIOU'
     consonants = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
-    ignoreds = ' '
-    vcount = ccount = ocount = 0
+    ignoreds = ''
+    spaces = vcount = ccount = ocount = 0
     for char in text:
         if char in ignoreds:
             pass
+        elif char is ' ':
+            spaces += 1
         elif char in vowels:
             vcount += 1
         elif char in consonants:
@@ -36,23 +38,35 @@ def score_plaintext(text):
     #     return False
     # else:
     #     return vcount, ccount, ocount
-    return vcount, ccount, ocount
+
+    #return vcount, ccount, ocount
+
+    sentence=text.split(' ')
+    #print(text)
+
+    if len(sentence) > 4:
+        return text
+    else:
+        return False
 
 def xorloop(hextxt):
     strtxt = mrlh64.hex_to_string(hextxt)
     print("hextxt: ({})".format(hextxt))
-    for i in range(1, 256):
+    for i in range(1, 128):
         xorchr = chr(i)
         xortxt = '{:{fill}>{width}}'.format(xorchr, fill=xorchr, width=len(strtxt)+1)
         xorhex = mrlh64.string_to_hex(xortxt)
         candidate = mrlxor.hexxor(hextxt,xorhex)
+        #print(candidate)
         if candidate:
-            score = score_plaintext(candidate)
+            score = score_plaintext(mrlh64.hex_to_string(candidate))
             #print("{} score: {}".format(hex(i), score))
-            try:
-                print(mrlh64.hex_to_string(candidate))
-            except UnicodeEncodeError:
-                pass
+            if score:
+                try:
+                    print("{}: {}".format(xorchr, mrlh64.hex_to_string(candidate)))
+                except UnicodeEncodeError:
+                    # this usually only happens on Windows
+                    print("Junk characters (that couldn't be displayed") 
         # if score:
         #     print("{} score: ".format(hex(i), str(score)))
         # else:
