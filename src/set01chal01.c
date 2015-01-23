4893da940114e517ed74d7eec294bbb7ecccc3c4
Micah R Ledbetter <micah@doublelinepartners.com>
2015-01-23 12:24:18 -0600

    Make some fixes to chal01

--- src/set01chal01.c
+++ src/set01chal01.c
@@ -2,20 +2,35 @@
 
 #include "../common/common.h"
 
-
 int main(int argc, char *argv[]) {
     int ix; 
-    unsigned char *buf;
-    char *b64;
+    char *binstr=NULL;
+    unsigned char *buf=NULL;
     size_t buf_len;
 
-    for(ix=0; ix<argc; ix++) {
+    printf("Printing arguments (if any)...\n");
+    for(ix=1; ix<argc; ix++) {
+        printf("Input buffer '%s' is '%i' bytes\n", argv[ix], strlen(argv[ix]));
         buf_len = hex2buf(argv[ix], &buf);
-        b64 = buf2b64(buf, buf_len); 
-        printf("Hex '%s'\n -> B64 '%s'\n\n", argv[ix], b64);
+        printf("Hex '%s'\n -> B64 '%s'\n\n", argv[ix], buf2b64(buf, buf_len, &binstr));
     }
+    printf("Checking official checker sentence...\n");
 
+    char *mathexstr = 
+        "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    buf_len = hex2buf(mathexstr, &buf);
+    check(buf_len >=0, "hex2buf() failed? returned length was %i", buf_len);
+    printf("matasano hex string: \n%s\nHex'd & Base64'd: \n%s\n", 
+        mathexstr, buf2b64(buf, buf_len, &binstr));
 
+    free(buf);
+    free(binstr);
+    return 0;
 error:
+    free(buf);
+    free(binstr);
     return -1;
 }
+
+
+
