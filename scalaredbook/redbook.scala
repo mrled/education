59469e7d03257d9268dd580a53ac015054ea2708
Micah R Ledbetter <mrled@younix.us>
2016-02-04 00:02:40 -0600

    Scala Red Book chapter 2 complete

--- scalaredbook/redbook.scala
+++ scalaredbook/redbook.scala
@@ -53,9 +53,54 @@ object ChapterTwo {
         loop(0)
     }
 
+    // parameter f: a function that takes an A argument and a B argument and returns:
+    //     a C value
+    // return value: a function that takes an A argument and returns:
+    //     a function that takes a B argument and returns: 
+    //         a C value
+    def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+        (a: A) => ((b: B) => f(a, b))
+    }
+
+    // f: A => B => C       ...is the same as: 
+    // f: A => (B => C)     ...because "the => operator associates right"
+    // ... fuckers! this adds no clarity! why would you allow it at all! 
+    // parameter f: a function that takes an A argument and returns:
+    //     a function that takes a B argument and returns: 
+    //         a C value
+    // return value: a function that takes an A argument and a B argument and returns:
+    //     a C value
+    def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+        //(a: A, b: B) => (val g = f(a); g(b))
+        (a: A, b: B) => f(a)(b)
+    }
+
+    def curryUncurryDemo(): Unit = {
+
+        val dumbExample = (i: Int, l: Long) => "int:" + i.toString() + " long:" + l.toString()
+        println("dumbExample:       " + dumbExample)
+        println("dumbExample(1,2):  " + dumbExample(1, 2))
+
+        val curriedExample = curry(dumbExample)
+        println("curriedExample:    " + curriedExample)
+        val stage1 = curriedExample(1)
+        println("stage1:            " + stage1)
+        val result = stage1(2)
+        println("result:            " + result)
+
+        val uncurried = uncurry(curriedExample)
+        println("uncurried:         " + uncurried)
+        println("uncurried(1,2):    " + uncurried(1, 2))
+    }
+
+    def compose[A,B,C](f: B => C, g: A => B): A => C = {
+        (a: A) => f(g(a))
+    }
+
     def main(args: Array[String]): Unit = {
         fibDemo()
         isSortedDemo()
+        curryUncurryDemo()
     }
 
 }
